8d846207cc5138e9ec5f6f11d65898aa8bec66e2
ulloa-g <ulloasaavedragabriel@gmail.com>
2023-10-14 18:32:46 -0300

    drop NaN from the price column

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import numpy as np
 
-data_set_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
-df = pd.read_csv(data_set_url, header=None)
+PATH = "/Users/ulloa/programing/projects/d_science_car_price/cars.csv"
+DATA_SET_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
+
+df = pd.read_csv(DATA_SET_URL, header=None)
 headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style", "drive-wheels", "engine-location", "wheel-base", "length", "width", "height",
            "curb-weight", "engine-type", "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg", "highway-mpg", "price"]
 df.columns = headers
-df.to_csv("/Users/ulloa/programing/projects/d_science_car_price/cars.csv")
+df1 = df.replace('?', np.NaN)
+df = df1.dropna(subset=["price"], axis=0)
+df.to_csv(PATH, index=False)  # index=False mean the row names will not be written
