b8aa50205e768d689eec6ae82f9d1c533e7190cc
ulloa-g <ulloasaavedragabriel@gmail.com>
2023-10-14 17:38:57 -0300

    add properly headers and save progress locally on csv file

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 data_set_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 df = pd.read_csv(data_set_url, header=None)
+headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style", "drive-wheels", "engine-location", "wheel-base", "length", "width", "height",
+           "curb-weight", "engine-type", "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg", "highway-mpg", "price"]
+df.columns = headers
+df.to_csv("/Users/ulloa/programing/projects/d_science_car_price/cars.csv")
