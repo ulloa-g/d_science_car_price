685d2bdbe65c3e047917665eab087145a3783f31
ulloa-g <ulloasaavedragabriel@gmail.com>
2023-10-17 16:33:19 -0300

    pre-processing data - data wrangling

--- app.py
+++ app.py
@@ -4,10 +4,44 @@ import numpy as np
 PATH = "/Users/ulloa/programing/projects/d_science_car_price/cars.csv"
 DATA_SET_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 
+
+def show_missing_data(dataframe):
+    missing_data = dataframe.isnull()
+    for c in missing_data.columns.values.tolist():
+        print(missing_data[c].value_counts())
+    return
+
+
 df = pd.read_csv(DATA_SET_URL, header=None)
 headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style", "drive-wheels", "engine-location", "wheel-base", "length", "width", "height",
            "curb-weight", "engine-type", "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg", "highway-mpg", "price"]
 df.columns = headers
-df1 = df.replace('?', np.NaN)
-df = df1.dropna(subset=["price"], axis=0)
-df.to_csv(PATH, index=False)  # index=False mean the row names will not be written
+df = df.replace('?', np.NaN)
+
+# replace by mean:
+avg_norm_loss = df["normalized-losses"].astype("float").mean(axis=0)
+avg_bore = df["bore"].astype("float").mean(axis=0)
+avg_stroke = df["stroke"].astype("float").mean(axis=0)
+avg_horsepower = df["horsepower"].astype("float").mean(axis=0)
+avg_peakrpm=df['peak-rpm'].astype('float').mean(axis=0)
+df["normalized-losses"].replace(np.nan, avg_norm_loss, inplace=True)
+df["bore"].replace(np.nan, avg_bore, inplace=True)
+df["stroke"].replace(np.nan, avg_stroke, inplace=True)
+df["horsepower"].replace(np.nan, avg_horsepower, inplace=True)
+df['peak-rpm'].replace(np.nan, avg_peakrpm, inplace=True)
+
+# replace by frecuency:
+#print(df["num-of-doors"].value_counts())  # --> show count on every element
+#print(df["num-of-doors"].value_counts().idxmax())  # --> calculate the most frecuent one automatically
+df["num-of-doors"].replace(np.nan, "four", inplace=True)
+
+# drop the whole row:
+df = df.dropna(subset=["price"], axis=0)
+
+# print(df.dtypes)  # --> we need to change some datatypes here:
+df[["bore", "stroke"]] = df[["bore", "stroke"]].astype("float")
+df[["normalized-losses"]] = df[["normalized-losses"]].astype("int")
+df[["price"]] = df[["price"]].astype("float")
+df[["peak-rpm"]] = df[["peak-rpm"]].astype("float")
+# index=False mean the row names will not be written
+df.to_csv(PATH, index=False)
